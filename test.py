f7139a9d7c38eec69062d13ee6b698fba077f92f
alprival <alexprivalov@outlook.fr>
2023-10-15 16:04:28 +0200

    startup

---
+++ test.py
@@ -0,0 +1,9 @@
+
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+with open('dataset_test.csv', newline='') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         print(', '.join(row))
